ca00b1928a6b04c40297388b7daf3501b281134f
PaulGamerBoy101 <166418808+PaulGamerBoy101@users.noreply.github.com>
2025-03-05 15:54:40 -0700

    Update startup.js

--- src/js/startup.js
+++ src/js/startup.js
@@ -3,14 +3,6 @@ fetch('../package.json')
   .then(res => {
     version = res.version;
     createTab();
-    fetch('https://api.github.com/repos/MystPi/ninetails/releases/latest')
-      .then(res => res.json())
-      .then(res => {
-        if (res.tag_name !== 'v' + version) {
-          byId('update-available-version').innerText = res.tag_name.slice(1);
-          byId('update-available').style.display = 'block';
-        }
-      });
   });
 
     document.title = "101 Tales"; 
